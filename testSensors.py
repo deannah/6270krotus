57012193d5013dafe1136e00be5a77fe1eccf669
Deanna Heer <deannah@mit.edu>
2015-01-30 01:45:53 -0500

    Sensor Test actually works.

--- testSensors.py
+++ testSensors.py
@@ -7,15 +7,15 @@ import time
 
 def testSensors(model):
 	model.getGyroData()
-	model.getLeftBumpData()
-	model.getRightBumpData()
+	#model.getLeftBumpData()
+	#model.getRightBumpData()
 
 model = RobotModel()
 
 try:
 	while True:
 		testSensors(model)
-		time.sleep(.1)
+		time.sleep(.05)
 
 except KeyboardInterrupt:
 	print "Ending test session"
